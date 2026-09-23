52675c26c3d4d515431d185a61fc5b04009b76e6
Eleven <nieshiyi20060927@qq.com>
2026-09-23 23:46:13 +0800

    ---

--- PTA作业-C语言/有序链表的合并/两个有序链表的合并.c
+++ PTA作业-C语言/有序链表的合并/两个有序链表的合并.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
-int main(){
+#include <stdlib.h>
+
+typedef struct Node{
+    int data;
+    struct Node* next;
+}Node;
+
+// 创建链表的函数 返回头节点
+// 尾插法
+Node* createList(){
+    Node* head = NULL;
+    Node* tail = NULL;
     
+}
+
+
+int main(){
+
     return 0;
 }
